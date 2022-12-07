dd4768028006b557d5b07e56e56f52927f95f64a
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-12-07 19:54:22 +0200

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+// import axios from 'axios';
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+  ul,h1,h2,h3,h4,h5,h6,li,p{list-style:none;margin:0;padding:0;};
+  body{
+   /* height: 100vh; */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40;
+        color: #010101;
+  }
+`;
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <GlobalStyle />
+      fbrbrb
     </div>
   );
 };
