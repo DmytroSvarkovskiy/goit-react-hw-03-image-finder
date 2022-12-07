ee2318f328db2743381c83729d65e338bb5b3fde
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-07 23:13:40 +0200

    -

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 // import axios from 'axios';
+// import { ThreeDots } from 'react-loader-spinner';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { Button } from '../Button/Button';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Wrapper } from './App.styled';
 import { Component } from 'react';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { Loader } from 'components/Loader/Loader';
 
 export class App extends Component {
   render() {
@@ -13,6 +15,7 @@ export class App extends Component {
         <GlobalStyle />
         <Searchbar />
         <ImageGallery></ImageGallery>
+        <Loader />
         <Button />
       </Wrapper>
     );
