63fc8e89b80c201fe2d58b2200a89557ff931df8
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-08 10:48:42 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import { GalleryList } from './ImageGallery.styled';
 export const ImageGallery = () => {
   return (
     <GalleryList>
+      кноакоапо
       <ImageGallaryItem />
     </GalleryList>
   );
