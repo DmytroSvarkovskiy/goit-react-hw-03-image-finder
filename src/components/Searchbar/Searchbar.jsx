bb30671e4e9fc3cabadd789f4d1af4d98be78482
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-12-09 17:42:33 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -7,15 +7,16 @@ import {
   Label,
 } from './Searchbar.styled';
 import { BiSearch } from 'react-icons/bi';
-// import PropTypes from 'prop-types';
 const initialValues = {
   search: '',
 };
 
 export const Searchbar = ({ onSubmit }) => {
   const submitSearch = (value, { resetForm }) => {
-    onSubmit(value.search);
-    resetForm();
+    if (value.search) {
+      onSubmit(value.search);
+      resetForm();
+    }
   };
   return (
     <SearchbarHeader>
